67d65e9ff28098acc4f45ea252eb624ce07e0fd6
adnansakel <adnan.sakel@gmail.com>
2020-04-19 23:14:43 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { finalize, tap, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'my-app',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  result = 'test';
+  selectedFile: File;
+  constructor(private http: HttpClient) { }
+
+  test(){
+    this.result = 'test2';
+  }
+
+  onFileChanged(event){
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = () => { // called once readAsDataURL is completed
+        this.selectedFile = reader.result;
+      }
+    }
+  }
+
+  getImageClass(){
+    this.http.get(`http://ec2-18-223-255-7.us-east-2.compute.amazonaws.com/hello/`, {responseType: 'json'})
+      .subscribe(res => console.log(res.toString));
+  }
+  
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+
 
 @Component({
   selector: 'hello',
@@ -7,4 +8,5 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent  {
   @Input() name: string;
+  
 }
